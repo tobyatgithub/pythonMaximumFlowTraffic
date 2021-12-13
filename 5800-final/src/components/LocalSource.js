6a3bb1d20301acd6313903bd1e254fbaf0ca008d
Boxuan Hu <hu.box@northeastern.edu>
2021-12-13 00:07:59 -0800

    line weight done

--- 5800-final/src/components/LocalSource.js
+++ 5800-final/src/components/LocalSource.js
@@ -1,8 +1,45 @@
 import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Polyline,
+} from "react-leaflet";
 import { Icon } from "leaflet";
 import "./local.css";
-import * as parkData from "../data/skateboard-parks.json";
+import * as parkData from "../data/vancouver-locations.json";
+import * as symboLines from "../data/vancouver-symbol";
+import { Button } from "react-bootstrap";
+
+// const positionsA = [{ B: 26, C: 34, D: 28 }];
+// const positionsB = [{ A: 38, C: 29, E: 42 }];
+// const positionsC = [{ A: 23, B: 25, F: 42, G: 37 }];
+// const positionsD = [{ A: 30, F: 38 }];
+// const positionsE = [{ B: 41, H: 38, I: 22, J: 33 }];
+// const positionsF = [{ C: 48, D: 38, G: 44 }];
+// const positionsG = [{ C: 34, F: 44, J: 44, K: 50 }];
+// const positionsH = [{ E: 38 }];
+// const positionsI = [{ E: 28, J: 23 }];
+// const positionsJ = [{ E: 27, G: 41, I: 26, K: 61 }];
+// const positionsK = [{ G: 47, J: 52 }];
+const purpleOptions = { fillcolor: "red", color: "lime" };
+
+const symbolNameMap = new Map();
+parkData.features.map((park) =>
+  symbolNameMap.set(park.symbol, park.geometry.coordinates)
+);
+//{ "start": "A", "end": { "B": 26, "C": 34, "D": 28 } },
+const linePositions = [];
+symboLines.lines.map((line1) =>
+  line1.end.map((end) =>
+    linePositions.push([
+      symbolNameMap.get(line1.start),
+      symbolNameMap.get(end.name),
+      end.weight,
+    ])
+  )
+);
 
 export const sheep = new Icon({
   iconUrl: "/richard.png",
@@ -12,17 +49,23 @@ export const sheep = new Icon({
 const LocalSource = () => {
   const [activePark, setActivePark] = React.useState(null);
 
+  const handleClick = () => {};
+  console.log(symbolNameMap);
+
   return (
     <div>
-      <div>hello</div>
-      <Map center={[49.247123, -123.1416102]} zoom={13}>
+      <Button className="button" onClick={handleClick}>
+        Large button
+      </Button>
+      <MapContainer center={[49.247123, -123.1416102]} zoom={13}>
+        {/* <Circle center={[49.247123, -123.1416102]} radius={400} color="red" /> */}
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {parkData.features.map((park) => (
           <Marker
-            key={park.properties.PARK_ID}
+            key={park.name}
             position={[
               park.geometry.coordinates[0],
               park.geometry.coordinates[1],
@@ -31,6 +74,14 @@ const LocalSource = () => {
               setActivePark(park);
             }}
             icon={sheep}
+          />
+        ))}
+        {linePositions.map((l) => (
+          <Polyline
+            key={1}
+            pathOptions={purpleOptions}
+            positions={[l[0], l[1]]}
+            weight={l[2] * 0.2}
           />
         ))}
 
@@ -50,7 +101,7 @@ const LocalSource = () => {
             </div>
           </Popup>
         )}
-      </Map>
+      </MapContainer>
     </div>
   );
 };
--- 5800-final/src/components/RemoteSource.js
+++ 5800-final/src/components/RemoteSource.js
@@ -1,22 +1,45 @@
 import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
+import * as L from "leaflet";
+
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Polyline,
+  Polygon,
+  Circle,
+  Tooltip,
+} from "react-leaflet";
 import "./local.css";
-import * as parkData from "../data/skateboard-parks.json";
 import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 export default function RemoteSource() {
+  const purpleOptions = { fillcolor: "red", color: "lime" };
+  const redOptions = { fillcolor: "red", color: "lime" };
+
   const url =
     "https://data.police.uk/api/crimes-street/all-crime?lat=52.629829&lng=-1.131592&date=2019-10";
   const { data, error } = useSWR(url, { fetcher });
-  const crimes = data && !error ? data.slice(0, 100) : [];
-  const a = crimes.length;
-  const b = 1;
-  if (crimes.length == 0) return <div>loading...</div>;
+  const crimes = data && !error ? data.slice(0, 10) : [];
+
+  const positions3 = [];
+  crimes.map((crime) =>
+    positions3.push([crime.location.latitude, crime.location.longitude])
+  );
+  const positions = positions3.slice(1, 4);
+  const positions2 = positions3.slice(5, 9);
+  //   return <div>{positions}</div>;
+
+  //   const positions2 = [];
+  //   crimes.map((crime) =>
+  //     positions.push([crime.location.latitude, crime.location.longitude])
+  //   );
+
   return (
-    <Map center={[52.6376, -1.135171]} zoom={12}>
+    <MapContainer center={[52.6376, -1.135171]} zoom={12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -25,9 +48,14 @@ export default function RemoteSource() {
         <Marker
           key={crime.id}
           //   position={[crime.location.latitude, crime.location.longitude]}
-          position={[52.6376, -1.135171]}
+          position={[crime.location.latitude, crime.location.longitude]}
         />
       ))}
-    </Map>
+      <Polyline pathOptions={purpleOptions} positions={positions} />;
+      <Polyline color="red" weight="10" positions={positions2}>
+        <Tooltip permanent>sticky Tooltip for Polygon</Tooltip>
+      </Polyline>
+      <Polyline color="green" weight="5" positions={positions2}></Polyline>
+    </MapContainer>
   );
 }

94052a5521a7339fc4b8e40f5a759ef3570910ec
tobyatgithub <toby.fangyuan@gmail.com>
2021-12-12 18:18:53 -0800

    adding multi source example.

--- main/ford_fulkerson.py
+++ main/ford_fulkerson.py
@@ -5,15 +5,28 @@ from typing import List, Union
 Num = Union[int, float]
 DEBUG = True
 
-g = Graph(
-    {
-        "A": {"B": 16, "C": 13},
-        "B": {"C": 10, "D": 12},
-        "C": {"B": 4, "E": 14},
-        "D": {"C": 9, "F": 20},
-        "E": {"D": 7, "F": 4},
-    }
-)
+g = Graph({
+    "A": {
+        "B": 16,
+        "C": 13
+    },
+    "B": {
+        "C": 10,
+        "D": 12
+    },
+    "C": {
+        "B": 4,
+        "E": 14
+    },
+    "D": {
+        "C": 9,
+        "F": 20
+    },
+    "E": {
+        "D": 7,
+        "F": 4
+    },
+})
 print(g)
 
 
@@ -33,10 +46,8 @@ def fordBFS(cur_graph: Graph, source: str, sink: str) -> (bool, dict):
     while queue:
         cur = queue.pop(0)
         for neighbor in cur_graph.get_neighbors(cur):
-            if (
-                neighbor not in visited
-                and cur_graph.get_weight(fromNode=cur, toNode=neighbor) > 0
-            ):
+            if (neighbor not in visited and
+                    cur_graph.get_weight(fromNode=cur, toNode=neighbor) > 0):
                 queue.append(neighbor)
                 visited.add(neighbor)
                 parentTracker[neighbor] = cur
@@ -45,7 +56,8 @@ def fordBFS(cur_graph: Graph, source: str, sink: str) -> (bool, dict):
 
 
 # test ford fulkerson
-def fordFulkerson(in_graph: Graph, source: str, sink: str) -> ([int, float], Graph):
+def fordFulkerson(in_graph: Graph, source: str,
+                  sink: str) -> ([int, float], Graph):
     # part 1, init and pre-construction
     flag = True
     cur_graph, maxFlowGraph = Graph(in_graph.graph), Graph()
@@ -65,8 +77,8 @@ def fordFulkerson(in_graph: Graph, source: str, sink: str) -> ([int, float], Gra
         while cur != source:
             parent = parentTracker.get(cur, source)
             local_maxFlow = min(
-                local_maxFlow, cur_graph.get_weight(fromNode=parent, toNode=cur)
-            )
+                local_maxFlow, cur_graph.get_weight(fromNode=parent,
+                                                    toNode=cur))
             cur = parent
         if DEBUG:
             print(
@@ -123,15 +135,12 @@ def OR_maxFlow(
     print("")
     print("  Arc    Flow / Capacity")
     for i in range(max_flow.NumArcs()):
-        print(
-            "%1s -> %1s   %3s  / %3s"
-            % (
-                max_flow.Tail(i),
-                max_flow.Head(i),
-                max_flow.Flow(i),
-                max_flow.Capacity(i),
-            )
-        )
+        print("%1s -> %1s   %3s  / %3s" % (
+            max_flow.Tail(i),
+            max_flow.Head(i),
+            max_flow.Flow(i),
+            max_flow.Capacity(i),
+        ))
     print("Source side min-cut:", max_flow.GetSourceSideMinCut())
     print("Sink side min-cut:", max_flow.GetSinkSideMinCut())
     return max_flow
@@ -146,7 +155,6 @@ def OR_maxFlow(
 
 # OR_maxFlow(0, 4, start_nodes, end_nodes, capacities)
 
-
 ## test ff
 test1 = fordFulkerson(g, "A", "F")
 print(test1)
@@ -157,6 +165,7 @@ print("**" * 10)
 def turnGraphToNodesLists(graph):
     vertices = graph.get_vertexs()
     s_node, e_node, cap = [], [], []
+    mapping = []
 
     for index, vertex in enumerate(vertices):
         neightbors = graph.get_neighbors(vertex)
@@ -164,12 +173,86 @@ def turnGraphToNodesLists(graph):
             s_node.append(index)
             e_node.append(vertices.index(node))
             cap.append(graph.get_weight(vertex, node))
-    return s_node, e_node, cap
+        mapping.append(vertex)
+    return s_node, e_node, cap, mapping
 
 
-# # web example with result from 0 to 4 == 23.
-# start_nodes = [0, 0, 0, 1, 1, 2, 2, 3, 3]
-# end_nodes = [1, 2, 3, 2, 4, 3, 4, 2, 4]
-# capacities = [20, 30, 10, 40, 30, 10, 20, 5, 20]
-start_nodes, end_nodes, capacities = turnGraphToNodesLists(g)
+start_nodes, end_nodes, capacities, mapping = turnGraphToNodesLists(g)
 OR_maxFlow(0, 5, start_nodes, end_nodes, capacities)
+
+g = Graph({
+    "Waterfront": {
+        "South Granville": 26,
+        "Mount Pleasant": 34,
+        "PNE": 28
+    },
+    "South Granville": {
+        "Waterfront": 38,
+        "Mount Pleasant": 29,
+        "Marine Gate": 42
+    },
+    "Mount Pleasant": {
+        "Waterfront": 23,
+        "South Granville": 25,
+        "Brentwood ": 42,
+        "Metrotown": 37
+    },
+    "PNE": {
+        "Waterfront": 30,
+        "Brentwood ": 38
+    },
+    "Marine Gate": {
+        "South Granville": 41,
+        "Airport": 38,
+        "Richmood": 22,
+        "E-W Connector": 33
+    },
+    "Brentwood ": {
+        "Mount Pleasant": 48,
+        "PNE": 38,
+        "Metrotown": 44
+    },
+    "Metrotown": {
+        "Mount Pleasant": 34,
+        "Brentwood ": 44,
+        "E-W Connector": 44,
+        "New West": 50
+    },
+    "Airport": {
+        "Marine Gate": 38
+    },
+    "Richmood": {
+        "Marine Gate": 28,
+        "E-W Connector": 23
+    },
+    "E-W Connector": {
+        "Marine Gate": 27,
+        "Metrotown": 41,
+        "Richmood": 26,
+        "New West": 61
+    },
+    "New West": {
+        "Metrotown": 47,
+        "E-W Connector": 52
+    }
+})
+"""
+A = Waterfront, B = South Granville, C = Mount Pleasant, D = PNE, 
+E = Marine Gate, F = Brentwood, G = Metrotown, H = Airport, I = Richmood, 
+J = Temp, K = New West
+"""
+
+# connect multiple sources to one
+g.add_edge("source", "Richmond", 500)
+g.add_edge("source", "Metrotown", 500)
+g.add_edge("source", "Marine Gate", 500)
+
+print("=test2=" * 10)
+test2 = fordFulkerson(g, "source", "Waterfront")
+print(test2)
+
+print("**" * 10)
+
+start_nodes, end_nodes, capacities, mapping = turnGraphToNodesLists(g)
+print("mapping = ", mapping, len(mapping))
+OR_maxFlow(12, 11, start_nodes, end_nodes, capacities)
